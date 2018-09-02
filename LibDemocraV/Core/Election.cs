0cd54a23e6ac1b4177352ed9bd9666d4ea0f39ee
John Moser <john.r.moser@gmail.com>
2018-09-02 09:11:57 -0400

    Update business name

--- LibDemocraV/Core/Election.cs
+++ LibDemocraV/Core/Election.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) Moonset Technology Holdings, LLC. All rights reserved.  
+// Copyright (c) Moonset Technologies, LLC. All rights reserved.  
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.  
 //
 
