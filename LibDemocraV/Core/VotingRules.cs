b193cf19852c5b797ec4d5f6b1d2e6e9c0ea1260
John Moser <john.r.moser@gmail.com>
2018-09-03 13:12:11 -0400

    Begin shaping the VotingRule class.

---
+++ LibDemocraV/Core/VotingRules.cs
@@ -0,0 +1,20 @@
+﻿//
+// Copyright (c) Moonset Technology Holdings, LLC. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoonsetTechnologies.Voting.Factories;
+
+namespace MoonsetTechnologies.Voting.VotingRules 
+{
+    public abstract class VotingRule
+    {
+        private AbstractBallotFactory BallotFactory { get; }
+
+        public Ballot GetNewBallot() => BallotFactory.CreateBallot();
+        public Ballot CopyBallot(ReadOnlyBallot ballot) => BallotFactory.CopyBallot(ballot);
+    }
+}
