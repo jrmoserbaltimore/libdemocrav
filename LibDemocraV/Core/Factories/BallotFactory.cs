0c7e4b5ae89b937e5dcd43664a9886c1c303b643
John Moser <john.r.moser@gmail.com>
2018-09-03 13:11:38 -0400

    Begin creating factories.

---
+++ LibDemocraV/Core/Factories/BallotFactory.cs
@@ -0,0 +1,23 @@
+﻿//
+// Copyright (c) Moonset Technology Holdings, LLC. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MoonsetTechnologies.Voting.Factories
+{
+    /// <summary>
+    /// Base class for implementing a factory for ballots.
+    /// </summary>
+    public abstract class AbstractBallotFactory
+    {
+        public abstract Ballot CreateBallot();
+        public abstract Ballot CopyBallot(ReadOnlyBallot ballot);
+        public abstract Vote CreateVote();
+    }
+
+
+}
