4105b0bfb4d594186dc9624fec3ba772524eb3d2
John Moser <john.r.moser@gmail.com>
2019-04-08 07:30:28 -0400

    Comments

--- LibDemocraV/Core/Tabulators/TidemansAlternativeTabulator.cs
+++ LibDemocraV/Core/Tabulators/TidemansAlternativeTabulator.cs
@@ -17,9 +17,24 @@ namespace MoonsetTechnologies.Voting.Tabulators
         protected List<Candidate> candidates;
         public IEnumerable<Candidate> Candidates => candidates;
 
+        /// <inheritdoc/>
         public void TabulateRound()
         {
             throw new NotImplementedException();
+
+            // General algorithm:
+            //   Set is SmithSet or SchwartzSet
+
+            //   if Set is One Candidate
+            //     Winner is Candidate in Set
+            //   else if Set is All Candidates
+            //     Eliminate Candidate with Fewest Votes
+            //   else
+            //     Eliminate Candidates Not In Set
+
+            // Variants eliminating non-Smith and not non-Schwartz candidates:
+            //   Schwartz Winner:
+            //     When the Schwartz Set is one, elect that candidate.
         }
     }
 }
