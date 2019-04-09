de9298774cfaac52fb404cc44c280929a26be199
John Moser <john.r.moser@gmail.com>
2019-04-08 22:44:47 -0400

    Add SCC search to compute Smith and Schwartz

--- LibDemocraV/Core/Analytics/TopCycle.cs
+++ LibDemocraV/Core/Analytics/TopCycle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using MoonsetTechnologies.Voting.Analytics;
 
 namespace MoonsetTechnologies.Voting.Tabulators
@@ -83,8 +84,54 @@ namespace MoonsetTechnologies.Voting.Tabulators
                 // Find every SCC that cannot be reached by any other SCC.
                 // In the Smith Set, this is one SCC; in the Schwartz Set,
                 // we may have several.
+                Dictionary<(int, int), bool> reachable = new Dictionary<(int, int), bool>();
+                List<int> dominating = new List<int>();
+                List<Candidate> output = new List<Candidate>();
 
-                throw new NotImplementedException();
+                // Special thanks to https://stackoverflow.com/a/55526085/5601193
+                foreach (Candidate k in linkId.Keys)
+                {
+                    foreach(Candidate l in linkId.Keys)
+                    {
+                        foreach (Candidate m in linkId.Keys)
+                        {
+                            // The SCC containing (l) can reach the SCC containing (m) if
+                            //  - (l) defeats (m)
+                            //  - (l) ties with (m) and it's the Smith Set
+                            //  - (l) is already known to reach (m)
+                            //  - (l) can reach (k) and (k) can reach (m)
+                            reachable[(linkId[l], linkId[m])] =
+                                graph.Wins(l).Contains(m) ||
+                                (isSmith && graph.Ties(l).Contains(m)) ||
+                                reachable[(linkId[l], linkId[m])] ||
+                                (reachable[(linkId[l], linkId[k])] && reachable[(linkId[k], linkId[m])]);
+                        }
+                    }
+                }
+
+                // Time to find all dominating SCCs
+                dominating.AddRange(linkId.Values.Distinct());
+                foreach (int j in linkId.Values.Distinct())
+                {
+                    foreach (int k in linkId.Values.Distinct())
+                    {
+                        // Reaching itself doesn't count
+                        if (j == k)
+                            continue;
+                        // If we can reach j from k, j is not dominating
+                        if (reachable[(k, j)])
+                            dominating.Remove(j);
+                    }
+                }
+
+                // Select all the candidates from the SCCs
+                foreach (Candidate c in linkId.Keys)
+                {
+                    if (dominating.Contains(linkId[c]))
+                        output.Add(c);
+                }
+
+                return output;
             }
 
             // Must compute SmithSet first
