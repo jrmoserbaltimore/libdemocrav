2ecf1d47a8289ae82e1f247479bb54c0b3cda4d1
John Moser <john.r.moser@gmail.com>
2019-05-02 19:20:37 -0400

    Begun work on abstract factories

--- Extensions/RankedChoiceVoting/src/RankedBallotFactory.cs
+++ Extensions/RankedChoiceVoting/src/RankedBallotFactory.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using MoonsetTechnologies.Voting;
+
 
 namespace MoonsetTechnologies.Voting.Utility
 {
     [BallotTypeId(typeof(RankedBallot))]
     public class RankedBallotFactory : AbstractBallotFactory
     {
+        protected List<Candidate> candidates = new List<Candidate>();
+        protected IEnumerable<Candidate> Candidates => candidates;
         public IBallot CreateBallot(IEnumerable<IRankedVote> votes) => new RankedBallot(votes);
         public override IBallot DecodeBallot(string encodedBallot)
         {
+            Regex r = new Regex(@"^(([\da-f]{8}(-[\da-f]{4}){3}-[\da-f]{12})([>=]|$))+");
+
             throw new NotImplementedException();
         }
         public override IBallot CreateBallot(IEnumerable<IVote> votes)
--- LibDemocraV/Core/AbstractBallotFactory.cs
+++ LibDemocraV/Core/AbstractBallotFactory.cs
@@ -7,6 +7,12 @@ namespace MoonsetTechnologies.Voting.Utility
     public abstract class AbstractBallotFactory
     {
         public abstract IBallot CreateBallot(IEnumerable<IVote> votes);
+        /// <summary>
+        /// Decode an encoded ballot 
+        /// </summary>
+        /// <param name="encodedBallot"></param>
+        /// <param name="candidates"></param>
+        /// <returns></returns>
         public abstract IBallot DecodeBallot(string encodedBallot);
     }
 }
