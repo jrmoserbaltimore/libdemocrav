4b12887d6221f3835f68cfd3083c5f60673d606c
John Moser <john.r.moser@gmail.com>
2019-05-03 12:30:59 -0400

    Finish tabulation iteration process

--- Tabulators/MeekSTVTabulator/src/MeekSTVTabulator.cs
+++ Tabulators/MeekSTVTabulator/src/MeekSTVTabulator.cs
@@ -32,6 +32,15 @@ namespace MoonsetTechnologies.Voting.Tabulators
         // Beyond ten is not generally necessary:
         // OpaVote uses 6, and the reference algorithm uses 9.
         private int precision = 9;
+        // Round up to precision
+        private decimal RoundUp(decimal d)
+        {
+            // The reference rule says to round UP, so we need
+            // to Round(weight + 10^(-precision) / 2).
+            decimal r = 0.5m * Convert.ToDecimal(Math.Pow(10.0D,
+                Convert.ToDouble(0 - precision)));
+            return decimal.Round(d + r, precision);
+        }
         private decimal omega = 0.000001m;
         private decimal quota = 0.0m;
         private List<IRankedBallot> Ballots;
@@ -56,14 +65,39 @@ namespace MoonsetTechnologies.Voting.Tabulators
         {
         }
 
+        protected bool IsComplete()
+        {
+            // Reference rule B.1
+            int e = 0, h = 0;
+            foreach (CandidateState c in candidates.Values)
+            {
+                if (c.State == CandidateState.States.elected)
+                    e++;
+                else if (c.State == CandidateState.States.hopeful)
+                    h++;
+            }
+            // We're done counting.  Go to reference rule step C.
+            if (e + h <= seats)
+            {
+                foreach (CandidateState c in candidates.Values)
+                {
+                    // Elect or defeat hopefuls.
+                    if (c.State == CandidateState.States.hopeful)
+                    {
+                        if (e == seats)
+                            c.State = CandidateState.States.defeated;
+                        else
+                            c.State = CandidateState.States.elected;
+                    }
+                }
+                return true;
+            }
+            // Still not done.
+            return false;
+        }
         // Reference rule B.2.a
         protected void DistributeVotes()
         {
-            // The reference rule says to round UP, so we need
-            // to Round(weight + 10^(-precision) / 2).
-            decimal r = 0.5m * Convert.ToDecimal(Math.Pow(10.0D,
-                Convert.ToDouble(0 - precision)));
-
             // Zero the vote counts
             foreach (CandidateState c in candidates.Values)
                 c.VoteCount = 0.0m;
@@ -83,7 +117,7 @@ namespace MoonsetTechnologies.Voting.Tabulators
                     // Get value to transfer to this candidate, restricted to
                     // the specified precision
                     decimal value = weight * candidates[v.Candidate].KeepFactor;
-                    weight = decimal.Round(weight + r, precision);
+                    weight = RoundUp(weight);
 
                     // Add this to the candidate's vote and remove from ballot's
                     //weight
@@ -136,8 +170,9 @@ namespace MoonsetTechnologies.Voting.Tabulators
         protected decimal ComputeSurplus()
         {
             decimal s = 0.0m;
-            // FIXME:  does this mean total surplus not less than zero,
-            // or only count candidates whose surplus is greater than zero?
+            // XXX:  does the reference rule mean total surplus not less
+            // than zero, or only count candidates whose surplus is greater
+            // than zero?
             foreach (CandidateState c in candidates.Values)
             {
                 if (c.State == CandidateState.States.elected)
@@ -146,6 +181,35 @@ namespace MoonsetTechnologies.Voting.Tabulators
             if (s < 0.0m)
                 s = 0.0m;
             return s;
+        }
+
+        protected bool UpdateKeepFactors()
+        {
+            // If no KeepFactors change or if any Keepfactor increases,
+            // we have a stable state and are in stasis.
+            bool noChange = true;
+            bool increase = false;
+            foreach (CandidateState c in candidates.Values)
+            {
+                decimal kf = c.KeepFactor;
+                // XXX: does the reference rule intend we round up
+                // between each operation, or multiply by (q/v) and
+                // then round up?
+                c.KeepFactor *= quota / c.VoteCount;
+                c.KeepFactor = RoundUp(c.KeepFactor);
+                if (kf != c.KeepFactor)
+                    noChange = false;
+                if (c.KeepFactor > kf)
+                    increase = true;
+            }
+
+            // Stasis detect
+            return (noChange || increase);
+        }
+
+        protected void EliminateCandidates()
+        {
+            throw new NotImplementedException();
         }
 
         protected bool DetectStasis()
@@ -158,7 +222,13 @@ namespace MoonsetTechnologies.Voting.Tabulators
 
         public IEnumerable<Candidate> Candidates => candidates.Keys;
 
-        public bool Complete => throw new NotImplementedException();
+        // It's okay to use IsComplete() here because it will only
+        // change state when there are no more rounds to tabulate.
+        // This only occurs in a special case where the election has
+        // no more candidates in total than seats and IsComplete() is
+        // called before TabulateRound();
+        /// <inheritdoc/>
+        public bool Complete => IsComplete();
 
         public void TabulateRound()
         {
@@ -166,28 +236,45 @@ namespace MoonsetTechnologies.Voting.Tabulators
             // We implement this by having surplus greater than the
             // number of whole ballots.
             decimal surplus = Convert.ToDecimal(Ballots.Count) + 1;
-            // XXX:  B.1 Test Count complete
+
+            // B.1 Test Count complete
+            if (IsComplete())
+                return;
 
             do
             {
                 decimal s = surplus;
                 bool elected = false;
+                bool kfStasis = true;
+                // B.2.a
                 DistributeVotes();
+                // B.2.b
                 quota = ComputeQuota();
+                // B.2.c
                 elected = ElectWinners();
+                // B.2.d
                 surplus = ComputeSurplus();
-                // Counting continues at B1, next tabulation round.
+                // B.2.e Counting continues at B1, next tabulation round.
                 if (elected)
                     return;
-                // Iteration complete, no election, defeat candidates
+                // B.2.e Iteration complete, no election, go to B.3
                 else if (surplus >= s || omega > surplus)
                     break;
-                // XXX:  B.2.f Update keep factors.
-
-                // XXX:  Detect stasis.
+                // B.2.f Update keep factors.  Also detect stasis.
+                kfStasis = UpdateKeepFactors();
+                // Upon Stasis, we must eliminate candidates (B.3)
+                // We know:
+                //   - Elected < Seats
+                //   - Elected + Hopeful > Seats
+                // Therefor IsComplete() is false and will not change state.
+                // Re-entering this body is guaranteed to return to this state.
+                // Elimination is required.
+                if (kfStasis)
+                    break;
             } while (true);
-            // XXX:  B.3 Defeat low candidates
-            throw new NotImplementedException();
+            // B.3 Defeat low candidates
+            EliminateCandidates();
+            // B.4:  Next call enters at B.1
         }
     }
 }
