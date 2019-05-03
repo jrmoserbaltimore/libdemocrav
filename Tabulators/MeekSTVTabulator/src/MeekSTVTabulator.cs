e706950437c413c9f4ad5a5e6dc77ec10facc8e9
John Moser <john.r.moser@gmail.com>
2019-05-02 22:05:44 -0400

    Added MEek STV functions for votes and quota

--- Tabulators/MeekSTVTabulator/src/MeekSTVTabulator.cs
+++ Tabulators/MeekSTVTabulator/src/MeekSTVTabulator.cs
@@ -26,12 +26,20 @@ namespace MoonsetTechnologies.Voting.Tabulators
     {
         // number to elect
         private int seats;
-        // Number of decimal places
+        // Number of decimal places. The decimal type avoids
+        // rounding error for up to 27 digits. For 9 billion
+        // votes, precision up to 17 decimal places is possible.
+        // Beyond ten is not generally necessary:
+        // OpaVote uses 6, and the reference algorithm uses 9.
         private int precision = 9;
         private decimal omega = 0.000001m;
+        private decimal quota = 0.0m;
+        private List<IRankedBallot> Ballots;
+
+        private Dictionary<Candidate, CandidateState> candidates;
+
         private class CandidateState
         {
-            public Candidate Candidate;
             public enum States
             {
                 defeated = 0,
@@ -48,11 +56,76 @@ namespace MoonsetTechnologies.Voting.Tabulators
         {
         }
 
+        // Reference rule B.2.a
+        protected void DistributeVotes()
+        {
+            // The reference rule says to round UP, so we need
+            // to Round(weight + 10^(-precision) / 2).
+            decimal r = 0.5m * Convert.ToDecimal(Math.Pow(10.0D,
+                Convert.ToDouble(0 - precision)));
+
+            // Zero the vote counts
+            foreach (CandidateState c in candidates.Values)
+                c.VoteCount = 0.0m;
+
+            // Distribute the ballots among the votes.
+            // Meek also considered an alternative formulation in which
+            // voters would be allowed to indicate equal preference for
+            // some candidates instead of a strict ordering; we have not
+            // implemented this alternative.
+            foreach (IRankedBallot b in Ballots)
+            {
+                decimal weight = 1.0m;
+                List<IRankedVote> votes = new List<IRankedVote>(b.Votes);
+                votes.Sort();
+                foreach (IRankedVote v in votes)
+                {
+                    // Get value to transfer to this candidate, restricted to
+                    // the specified precision
+                    decimal value = weight * candidates[v.Candidate].KeepFactor;
+                    weight = decimal.Round(weight + r, precision);
+
+                    // Add this to the candidate's vote and remove from ballot's
+                    //weight
+                    candidates[v.Candidate].VoteCount += value;
+                    weight -= value;
+
+                    // Do this until weight hits zero, or we run out of rankings.
+                    // Note:  Already-elected candidates have keep factors between
+                    // 0 and 1; hopefuls all have 1; defeated will have 0.  The
+                    // remaining voting power is either all transfered to the first
+                    // hopeful candidate or exhausted as the ballot runs out of
+                    // non-defeated candidates.
+                    //
+                    // We only hit 0 if a non-elected hopeful is on the ballot.
+                    if (weight <= 0.0m)
+                        break;
+                }
+            }
+        }
+
+        // Reference rule B.2.b
+        protected void ComputeQuota()
+        {
+            decimal p = Convert.ToDecimal(Math.Pow(10.0D, Convert.ToDouble(0 - precision)));
+            quota = 0;
+            foreach (CandidateState c in candidates.Values)
+                quota += c.VoteCount;
+            quota /= seats + 1;
+            // truncate and add the precision digit
+            quota = decimal.Round(quota - 0.5m * p) + p;
+        }
+
+        // Reference rule B.2.c, returns true if hopefuls get elected.
+        protected bool ElectWinners()
+        {
+            throw new NotImplementedException();
+        }
         public IEnumerable<Candidate> SchwartzSet => throw new NotImplementedException();
 
         public IEnumerable<Candidate> SmithSet => throw new NotImplementedException();
 
-        public IEnumerable<Candidate> Candidates => throw new NotImplementedException();
+        public IEnumerable<Candidate> Candidates => candidates.Keys;
 
         public bool Complete => throw new NotImplementedException();
 
