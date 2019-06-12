9a20c5dbc5cfabdc41d8f9476cea1188e33abc19
John Moser <john.r.moser@gmail.com>
2019-06-11 21:47:18 -0400

    Fixed thread safety

--- LibDemocraV/Utility/ByNamePeopleFactory.cs
+++ LibDemocraV/Utility/ByNamePeopleFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,16 +12,20 @@ namespace MoonsetTechnologies.Voting.Utility
         {
         }
 
-        protected Dictionary<string, Person> Names { get; } = new Dictionary<string, Person>();
+        protected ConcurrentDictionary<string, WeakReference<Person>> Names { get; } = new ConcurrentDictionary<string, WeakReference<Person>>();
 
         /// <inheritdoc/>
         protected override Person FetchPerson(Person reference)
         {
-            Person p = reference;
-
-            if (!Names.TryGetValue(p.Name, out p))
-                p = Names[reference.Name] = reference;
-
+            Person p;
+            if (reference is null)
+                throw new ArgumentNullException("reference", "reference cannot be null!");
+            if (!(Names.TryGetValue(reference.Name, out WeakReference<Person> wp)
+                && wp.TryGetTarget(out p)))
+            {
+                Names[reference.Name] = new WeakReference<Person>(reference);
+                p = reference;
+            }
             return p;
         }
     }
