4e3fae46174c4a36ccfea336494a39ab8be78367
John Moser <john.r.moser@gmail.com>
2019-06-12 16:56:14 -0400

    working on tabulator options

---
+++ LibDemocraV/Tabulation/ITabulatorOption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoonsetTechnologies.Voting.Tabulation
+{
+    public interface ITabulatorOptionMetadata
+    {
+        string Description { get; }
+    }
+}
