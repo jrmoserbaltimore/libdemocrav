00c82d307a1ba27e679d5a6b21ca5dd92c17e617
John Moser <john.r.moser@gmail.com>
2019-06-19 15:28:10 -0400

    Begin reworking this as a tools pack

---
+++ DemocraV-Tools/Main.cs
@@ -0,0 +1,28 @@
+﻿using MoonsetTechnologies.Voting.Analytics;
+using MoonsetTechnologies.Voting.Ballots;
+using MoonsetTechnologies.Voting.Storage;
+using MoonsetTechnologies.Voting.Tabulation;
+using MoonsetTechnologies.Voting.Tiebreaking;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Threading.Tasks;
+
+namespace MoonsetTechnologies.Application
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args[0])
+            {
+                case "tabulate":
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("args", "Command is not available");
+            }
+        }
+    }
+}
---
+++ DemocraV-Tools/Tabulate.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MoonsetTechnologies.Application
+{
+    public class Tabulate
+    {
+        public void Main()
+        {
+            // TODO:  Parse arguments and tabulate an input file
+        }
+    }
+}
