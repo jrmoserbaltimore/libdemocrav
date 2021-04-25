38db5e8f144b69fdb688b5db77578474e1450cee
John Moser <john.r.moser@gmail.com>
2021-04-25 11:55:48 -0400

    Simplified IRV tabulator

--- Tabulators/InstantRunoffVotingTabulator/Tabulation/InstantRunoffVotingTabulator.cs
+++ Tabulators/InstantRunoffVotingTabulator/Tabulation/InstantRunoffVotingTabulator.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using MoonsetTechnologies.Voting.Analytics;
 using MoonsetTechnologies.Voting.Ballots;
 using MoonsetTechnologies.Voting.Tiebreaking;
@@ -8,8 +8,9 @@ using MoonsetTechnologies.Voting.Utility;
 
 namespace MoonsetTechnologies.Voting.Tabulation
 {
-    public class InstantRunoffVotingTabulator : RunoffTabulator
+    public class InstantRunoffVotingTabulator : AbstractTabulator
     {
+        /// <inheritdoc/>
         public InstantRunoffVotingTabulator(TabulationMediator mediator,
             AbstractTiebreakerFactory tiebreakerFactory,
             IEnumerable<ITabulatorSetting> tabulatorSettings)
@@ -17,15 +18,61 @@ namespace MoonsetTechnologies.Voting.Tabulation
         {
 
         }
-
-        /// <inheritdoc/>
-        protected override void InitializeTabulation(BallotSet ballots, IEnumerable<Candidate> withdrawn, int seats)
+        protected override void CountBallot(CountedBallot ballot)
         {
-            base.InitializeTabulation(ballots, withdrawn, seats);
+            // Only counts hopeful and elected candidates
+            Dictionary<Candidate, CandidateState> candidates
+                = candidateStates
+                   .Where(x => new[] { CandidateState.States.hopeful, CandidateState.States.elected }
+                     .Contains(x.Value.State))
+                   .ToDictionary(x => x.Key, x => x.Value);
 
-            RankedTabulationAnalytics analytics;
-            analytics = new RankedTabulationAnalytics(ballots, seats);
-            batchEliminator = new RunoffBatchEliminator(analytics, seats);
+            Vote vote = null;
+            foreach (Vote v in ballot.Votes)
+            {
+                // Skip candidates not included in this count.
+                if (!candidates.Keys.Contains(v.Candidate))
+                    continue;
+                // First vote examined or it beats current
+                if (vote is null || v.Beats(vote))
+                    vote = v;
+            }
+            if (!(vote is null))
+                candidateStates[vote.Candidate].VoteCount += ballot.Count;
+            else
+            {
+                // TODO:  Send an exhausted ballot event for counting purposes
+            }
+        }
+        // Finds the candidates with the lowest vote count
+        protected override IEnumerable<Candidate> GetEliminationCandidates()
+        {
+            decimal minVotes = candidateStates.Select(x => x.Value.VoteCount).Min();
+            return candidateStates.Where(x => x.Value.VoteCount == minVotes).Select(x => x.Key);
+        }
+
+        protected override TabulationStateEventArgs TabulateRound()
+        {
+            long tc = ballots.TotalCount();
+
+            Candidate winner = candidateStates.Where(x => x.Value.VoteCount > new decimal((tc / 2) + 0.5)).Select(x => x.Key).First();
+
+            if (!(winner is null))
+            {
+                SetState(winner, CandidateState.States.elected);
+            }
+            else
+            {
+                foreach (Candidate c in GetEliminationCandidates())
+                {
+                    SetState(c, CandidateState.States.defeated);
+                }
+            }
+            return new TabulationStateEventArgs
+            {
+                CandidateStates = CandidateStatesCopy,
+                Note = ""
+            };
         }
     }
 }
