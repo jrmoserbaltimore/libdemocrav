51583b071051aa3520514d3d62fe5a40aeb040b3
John Moser <john.r.moser@gmail.com>
2021-04-25 13:36:21 -0400

    Scaffold Minimax tabulator

---
+++ Tabulators/MinimaxTabulator/Tabulation/MinimaxTabulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using MoonsetTechnologies.Voting.Ballots;
+using MoonsetTechnologies.Voting.Utility;
+using MoonsetTechnologies.Voting.Analytics;
+
+namespace MoonsetTechnologies.Voting.Tabulation
+{
+    // Only use Minimax as Smith/Minimax
+    // TODO:  base on an abstract pairwise tabulator that initializes the graph
+    public class MinimaxTabulator : AbstractTabulator
+    {
+        /// <inheritdoc/>
+        public MinimaxTabulator(TabulationMediator mediator,
+            AbstractTiebreakerFactory tiebreakerFactory,
+            IEnumerable<ITabulatorSetting> tabulatorSettings)
+            : base(mediator, tiebreakerFactory, tabulatorSettings)
+        {
+
+        }
+        protected override void CountBallot(CountedBallot ballot)
+        {
+            // Only counts hopeful and elected candidates
+            Dictionary<Candidate, CandidateState> candidates
+                = candidateStates
+                   .Where(x => new[] { CandidateState.States.hopeful, CandidateState.States.elected, CandidateState.States.defeated }
+                     .Contains(x.Value.State))
+                   .ToDictionary(x => x.Key, x => x.Value);
+
+            // TODO:  Create a graph
+        }
+
+        protected override TabulationStateEventArgs TabulateRound()
+        {
+            // TODO:  If Smith setting is enabled, eliminate all non-Smith candidates first
+            // TODO:  Generate a list of largest pairwise defeats per candidate
+            // TODO:  Elect candidate who has the smallest pairwise defeat
+            return new TabulationStateEventArgs
+            {
+                CandidateStates = CandidateStatesCopy,
+                Note = ""
+            };
+        }
+    }
+}
---
+++ Tabulators/MinimaxTabulator/Utility/MinimaxTabulatorFactory.cs
@@ -0,0 +1,47 @@
+﻿using MoonsetTechnologies.Voting.Analytics;
+using MoonsetTechnologies.Voting.Tabulation;
+using MoonsetTechnologies.Voting.Tiebreaking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MoonsetTechnologies.Voting;
+using MoonsetTechnologies.Voting.Ballots;
+using System.Composition;
+
+namespace MoonsetTechnologies.Voting.Utility
+{
+    // General algorithm:
+    //   if CondorcetSet is One Candidate
+    //     Winner is Candidate in CondorcetSet
+    //   else
+    //     Eliminate Candidates not in RetainSet
+    //     Eliminate Candidate with Fewest Votes
+
+    // Variants as (CondorcetSet, RetainSet):
+    //   Tideman's Alternative:  (schwartz, smith)
+    //   Tideman's Alternative Schwartz:  (schwartz, schwartz)
+    //   Tideman's Alternative Smith:  (smith, smith)
+    public class MinimaxTabulatorFactory : AbstractTabulatorFactory<MinimaxTabulator>
+    {
+        public MinimaxTabulatorFactory()
+            : base()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void ConfigureTabulator(ITabulatorSetting tabulatorSetting)
+        {
+            base.ConfigureTabulator(tabulatorSetting);
+        }
+    }
+
+    [Export(typeof(ITabulatorSetting))]
+    [ExportMetadata("Title", "Smith-constrained")]
+    [ExportMetadata("Description", "Enable to use Smith/Minimax.")]
+    public class MinimaxTabulatorSmithSetting : TopCycleTabulatorSetting
+    {
+
+    }
+}
+
